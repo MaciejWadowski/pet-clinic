daf9563222161ab9bed1a019983695409a34da4a
wadziuxxx <macwadowski@tlen.pl>
2019-05-04 01:45:37 +0200

    Fixed Broken Links. Closes #45

--- pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/petclinic/controllers/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index() {
         return "index";
     }
+
+    @RequestMapping({"/find"})
+    public String bugHandler() {
+        return "notimplemented";
+    }
 }
