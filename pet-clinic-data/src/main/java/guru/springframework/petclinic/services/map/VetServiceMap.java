8ebd4dd33ff7d3c6fc07e9c8b40d47426126fe10
wadziuxxx <macwadowski@tlen.pl>
2019-05-22 14:13:06 +0200

    added pet type  sd jpa serice. closes #42

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/services/map/VetServiceMap.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.Set;
 
 @Service
+
 public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
 
     private final SpecialityService specialityService;
