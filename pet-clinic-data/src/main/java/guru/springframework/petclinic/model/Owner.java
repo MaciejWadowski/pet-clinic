0fca41ca33db7018510a8117f6fa2d3acbb480aa
wadziuxxx <macwadowski@tlen.pl>
2019-09-15 02:06:26 +0200

    add update operations for pet.Closes #59

--- pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Owner.java
@@ -1,6 +1,5 @@
 package guru.springframework.petclinic.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,11 +10,20 @@ import java.util.Set;
 
 @Setter
 @Getter
-@AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "owners")
 public class Owner extends Person {
+
+    @Builder
+    public Owner(Long id, String firstName, String lastName, String address, String city, String telephone, Set<Pet> pets) {
+        super(id, firstName, lastName);
+        this.address = address;
+        this.city = city;
+        this.telephone = telephone;
+        if (pets != null) {
+            this.pets = pets;
+        }
+    }
 
     @Column(name = "address")
     private String address;
@@ -31,5 +39,23 @@ public class Owner extends Person {
     private Set<Pet> pets = new HashSet<>();
     public Owner() {
         pets = new HashSet<>();
+    }
+
+    public Pet getPet(String name) {
+        return getPet(name, false);
+    }
+
+    public Pet getPet(String name, boolean ignoreNew) {
+        name = name.toLowerCase();
+        for (var pet : pets) {
+            if (!ignoreNew || !pet.isNew()) {
+                String compName = pet.getName();
+                compName = compName.toLowerCase();
+                if (compName.equals(name)) {
+                    return pet;
+                }
+            }
+        }
+        return null;
     }
 }
---
+++ pet-clinic-web/src/test/java/guru/springframework/petclinic/controllers/PetControllerTest.java
@@ -0,0 +1,108 @@
+package guru.springframework.petclinic.controllers;
+
+import guru.springframework.petclinic.model.Owner;
+import guru.springframework.petclinic.model.Pet;
+import guru.springframework.petclinic.model.PetType;
+import guru.springframework.petclinic.services.OwnerService;
+import guru.springframework.petclinic.services.PetService;
+import guru.springframework.petclinic.services.PetTypeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+class PetControllerTest {
+
+    @Mock
+    OwnerService ownerService;
+
+    @Mock
+    PetTypeService petTypeService;
+
+    @Mock
+    PetService petService;
+
+    @InjectMocks
+    PetController controller;
+
+    Owner owner;
+
+    Set<PetType> petTypes;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(controller)
+                .build();
+        owner = new Owner();
+        owner.setId(1L);
+        petTypes = new HashSet<>();
+        petTypes.add(PetType.builder().id(1L).type("Dog").build());
+        petTypes.add(PetType.builder().id(2L).type("Cat").build());
+    }
+
+    @Test
+    void initUpdateForm() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypes);
+        when(petService.findById(anyLong())).thenReturn(Pet.builder().id(2L).build());
+
+        mockMvc.perform(get("/owners/1/pets/2/edit"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(model().attributeExists("pet"))
+                .andExpect(view().name("pets/createOrUpdatePetForm"));
+    }
+
+    @Test
+    void initCreationForm() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypes);
+
+        mockMvc.perform(get("/owners/1/pets/new"))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("owner"))
+                .andExpect(model().attributeExists("pet"))
+                .andExpect(view().name("pets/createOrUpdatePetForm"));
+    }
+
+    @Test
+    void processCreationForm() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypes);
+
+        mockMvc.perform(post("/owners/1/pets/new"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"));
+
+        verify(petService).save(any());
+    }
+
+    @Test
+    void processUpdateForm() throws Exception {
+        when(ownerService.findById(anyLong())).thenReturn(owner);
+        when(petTypeService.findAll()).thenReturn(petTypes);
+
+        mockMvc.perform(post("/owners/1/pets/2/edit"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/owners/1"));
+
+        verify(petService).save(any());
+    }
+}
